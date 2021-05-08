3018a4330f37fcda02d0bf3e6445387b2ceae123
i'DLisT <i.dlist@outlook.com>
2021-05-08 13:59:36 +0800

    fix: wrong database logic

--- mysql.js
+++ mysql.js
@@ -1,5 +1,17 @@
 const { Database, Tables } = require('koishi-core')
 
+Database.extend('koishi-plugin-mysql', {
+  async loadGameData(subcommand, uid,) {
+    let res = await this.query(`SELECT save FROM \`${subcommand}_save\` WHERE uid = ${uid}`)
+    if (!res.length) return
+    else return JSON.stringify(res[0].save)
+  },
+  async saveGameData(subcommand, uid, save) {
+    return this.query(`INSERT INTO \`${subcommand}_save\` VALUES (${uid}, ${JSON.stringify(save)}) `
+      + `ON DUPLICATE KEY UPDATE save = ${JSON.stringify(save)}`)
+  }
+})
+
 module.exports = subcommand => {
   Database.extend('koishi-plugin-mysql', ({ Domain, tables }) => {
     tables[subcommand + '_save'] = {
@@ -9,16 +21,4 @@ module.exports = subcommand => {
   })
 
   Tables.config[subcommand + '_save'] = { primary: 'uid', unique: [] }
-
-  Database.extend('koishi-plugin-mysql', {
-    async loadGameData(subcommand, uid,) {
-      let res = await this.query(`SELECT save FROM \`${subcommand}_save\` WHERE uid = ${uid}`)
-      if (!res.length) return
-      else return JSON.stringify(res[0].save)
-    },
-    async saveGameData(subcommand, uid, save) {
-      return this.query(`INSERT INTO \`${subcommand}_save\` VALUES (${uid}, ${JSON.stringify(save)}) `
-        + `ON DUPLICATE KEY UPDATE save = ${JSON.stringify(save)}`)
-    }
-  })
 }
